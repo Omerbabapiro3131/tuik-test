ae872c808aff3201e77203814967d4ad386042db
Omerbabapiro3131 <omer02072016@gmail.com>
2026-09-08 13:16:32 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,12 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-app.use(express.static('public'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 app.get('/api/sorgula', async (req, res) => {
   const { isim, ilKodu } = req.query;
